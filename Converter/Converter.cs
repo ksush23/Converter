715be3e6a5cf130e74e46ee0ca91fec3d6c68f61
Ксюша <@DESKTOP-KGJ7JQN>
2019-09-16 19:32:34 +0300

    Добавьте файлы проекта.

---
+++ Converter/Converter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Converter
+{
+    class Converter
+    {
+        private double usd;
+        private double eur;
+
+        public Converter(double _usd, double _eur)
+        {
+            usd = _usd;
+            eur = _eur;
+        }
+        public void ConvertFromDollars(double hrn)
+        {
+            Console.Write("Dollars in hryvnas: ");
+            Console.WriteLine("{0:0.##}", (hrn / usd));
+        }
+        public void ConvertFromEuros(double hrn)
+        {
+            Console.Write("Euros in hryvnas: ");
+            Console.WriteLine("{0:0.##}", (hrn / eur));
+        }
+        public void ConvertToDollars(double hrn)
+        {
+            Console.Write("Hryvnas in dollars: ");
+            Console.WriteLine("{0:0.##}", (hrn * usd));
+        }
+        public void ConverttoEuros(double hrn)
+        {
+            Console.Write("Hryvnas in euros: ");
+            Console.WriteLine("{0:0.##}", (hrn * eur));
+        }
+    }
+}
---
+++ Converter/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double hrv;
+            do
+            {
+                Console.WriteLine("Enter how much hryvnas you have: ");
+                hrv = Convert.ToDouble(Console.ReadLine());
+            } while (hrv <= 0);
+
+            double usd;
+            do
+            {
+                Console.WriteLine("Enter convert course in dollars: ");
+                usd = Convert.ToDouble(Console.ReadLine());
+            } while (usd <= 0);
+
+            double eur;
+            do
+            {
+                Console.WriteLine("Enter convert course in euros: ");
+                eur = Convert.ToDouble(Console.ReadLine());
+            } while (eur <= 0);
+
+            Converter conv = new Converter(usd, eur);
+
+            conv.ConvertFromDollars(hrv);
+            conv.ConvertFromEuros(hrv);
+            conv.ConvertToDollars(hrv);
+            conv.ConverttoEuros(hrv);
+
+            Console.ReadKey();
+        }
+    }
+}
